fd9ef9f75cd4252eab41f079de191fae6d477757
danielleoanto16-debug <danielleoanto.16@gmail.com>
2026-04-13 16:06:05 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+const CACHE_NAME = 'calculadora-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instala el Service Worker y guarda los archivos básicos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Permite que la App responda incluso sin internet
 self.addEventListener('fetch', (event) => {
-  // Sin esto, Android no cree que es una App
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
